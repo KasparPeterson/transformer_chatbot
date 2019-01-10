87f7f600527dcacca3ee3d3fe77bbeda1d4a4ba0
Kaspar Peterson <kasparpeterson@hotmail.com>
2019-01-10 15:29:48 +0200

    Removes unnecessary log.

--- model/dataset.py
+++ model/dataset.py
@@ -109,5 +109,4 @@ class FacebookDataset(Dataset):
         y = [self.vocab.bos_id] + dialog[dialog_end-1] + [self.vocab.eos_id]
         y = y[:self.max_lengths]
 
-        print("__getitem__ persona_info: ", persona_info)
         return persona_info, h, y
