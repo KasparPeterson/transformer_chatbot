17cde751424624831d14284de958d2f079c260d8
Kaspar Peterson <kasparpeterson@hotmail.com>
2019-01-10 10:49:21 +0200

    Adds dockerfile to train agent.

---
+++ environment/docker_train.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IMAGE_NAME='transformer_chatbot'
+
+sudo docker run -it --rm \
+    -v $(pwd)/parameters:/workspace/parameters   \
+    -v $(pwd)/checkpoints:/workspace/checkpoints \
+    --network host --entrypoint "python" $IMAGE_NAME "train.py"
